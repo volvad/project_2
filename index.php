1070806c8f806c269473e8b2f936b542c47842ad
volvad <volgvad@gmail.com>
2022-09-05 12:47:55 +0400

    Add operations

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-echo "Hi"
-?>
+header("Content-Type: text/plain; charset=utf-8");
+
+echo "First number: ";
+$num1 = 20;
+echo "$num1 \n"; 
+
+echo "Second number: ";
+$num2 = 10;
+echo "$num2 \n";
+
+//Addition
+echo "Addition ";
+echo $num1 + $num2, "\n";
+
+//Subtraction
+echo "Subtraction ";
+echo $num1 - $num2, "\n";
+
+//Multiplikation
+echo "Multiplikation ";
+echo $num1 * $num2, "\n";
+
+//Division
+echo "Division ";
+echo $num1 / $num2, "\n";
