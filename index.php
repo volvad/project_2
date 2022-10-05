ee887d820dcecca867b2033656e550db214c84c0
volvad <volgvad@gmail.com>
2022-10-05 21:44:37 +0400

    Settings

--- index.php
+++ index.php
@@ -1,26 +1,37 @@
 <?php
-header("Content-Type: text/plain; charset=utf-8");
+// phpinfo();
 
-echo "First number: ";
-$num1 = 20;
-echo "$num1 \n"; 
+// header("Content-Type: text/plain; charset=utf-8");
 
-echo "Second number: ";
-$num2 = 10;
-echo "$num2 \n";
+// echo "First number: ";
+// $num1 = 20;
+// echo "$num1 \n";
 
-//Addition
-echo "Addition ";
-echo $num1 + $num2, "\n";
+// echo "Second number: ";
+// $num2 = 10;
+// echo "$num2 \n";
 
-//Subtraction
-echo "Subtraction ";
-echo $num1 - $num2, "\n";
+// //Addition
+// echo "Addition ";
+// echo $num1 + $num2, "\n";
 
-//Multiplikation
-echo "Multiplikation ";
-echo $num1 * $num2, "\n";
+// //Subtraction
+// echo "Subtraction ";
+// echo $num1 - $num2, "\n";
 
-//Division
-echo "Division ";
-echo $num1 / $num2, "\n";
+// //Multiplikation
+// echo "Multiplikation ";
+// echo $num1 * $num2, "\n";
+
+// //Division
+// echo "Division ";
+// echo $num1 / $num2, "\n";
+
+// $name = "Ivan";
+
+// function getName()
+// {
+//     global $name;
+//     echo $name;
+// }
+// getName();
